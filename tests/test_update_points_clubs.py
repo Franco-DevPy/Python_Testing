023e18b89c510a5734c95efe68795a23b9dee4dd
Franco-M <francomercau91@gmail.com>
2026-01-23 12:00:40 +0100

    Add test for updating points and booking places for clubs

---
+++ tests/test_update_points_clubs.py
@@ -0,0 +1,31 @@
+import server
+from server import app
+
+
+
+def test_update_points_clubs(): 
+    app.config["TESTING"] = True
+
+    client = app.test_client()
+
+    clubs = server.clubs
+    competitions = server.competitions
+
+    for club in clubs:
+        if club["name"] == "Iron Temple":
+            club["points"] = "10"
+
+    for comp in competitions:
+        if comp["name"] == "Spring Festival":
+            comp["numberOfPlaces"] = "5"
+            break
+
+    response = client.post('/purchasePlaces', data={
+        'competition': 'Spring Festival',
+        'club': 'Iron Temple',
+        'places': "3"
+    })
+
+    assert b"Great-booking complete!" in response.data
+    assert b"Points available: 7" in response.data
+
